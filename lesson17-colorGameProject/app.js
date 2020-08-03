b11052d9ed157cc1631bb584845941022e237d32
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-03 18:04:22 +0100

    colors array and pickedColor are now generated randomly

--- lesson17-colorGameProject/app.js
+++ lesson17-colorGameProject/app.js
@@ -1,18 +1,29 @@
 ((d) => {
 
-    let colors = [
-        "rgb(255, 0, 0)",
-        "rgb(255, 255, 0)",
-        "rgb(0, 255, 0)",
-        "rgb(0, 255, 255)",
-        "rgb(0, 0, 255)",
-        "rgb(255, 0, 255)",
-    ]
+    // generate a random RGB color
+    let randomColor = () => {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
 
+        return `rgb(${red}, ${green}, ${blue})`;
+    }
 
-    let squares = d.querySelectorAll(".square");
+    // make an array with "num" random colors
+    let generateRandomColors = (num) => {
+        let arr = []
+        for (let i = 0; i < num; i += 1) {
+            arr.push(randomColor());
+        }
+        return arr;
+    }
+
+    // store the generated random colors array in a vaiable
+    let colors = generateRandomColors(6);
 
     // change all squares to the pickedColor
+    let squares = d.querySelectorAll(".square");
+
     let changeColors = (color) => {
         for (let i = 0; i < squares.length; i += 1) {
             squares[i].style.backgroundColor = color;
@@ -20,6 +31,8 @@
     }
 
     // add initial colors to the squares
+    let header = d.querySelector("h1");
+
     for (let i = 0; i < squares.length; i += 1) {
         squares[i].style.backgroundColor = colors[i];     
 
@@ -32,6 +45,7 @@
             if (clickedColor === pickedColor) {
                 message.textContent = "Correct!";
                 changeColors(clickedColor);
+                header.style.backgroundColor = clickedColor;
             } else {
                 squares[i].style.backgroundColor = "#232323";
                 message.textContent = "Try again!";
