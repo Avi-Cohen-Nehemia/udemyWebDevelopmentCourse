1d074c541a89a4eb41071fa0ac891b2ee71f3967
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-03 17:12:17 +0100

    created all the divs and gave them a specific color

--- lesson17-colorGameProject/app.js
+++ lesson17-colorGameProject/app.js
@@ -1,4 +1,18 @@
 ((d) => {
 
+    let colors = [
+        "rgb(255, 0, 0)",
+        "rgb(255, 255, 0)",
+        "rgb(0, 255, 0)",
+        "rgb(0, 255, 255)",
+        "rgb(0, 0, 255)",
+        "rgb(255, 0, 255)",
+    ]
+
+    let squares = d.querySelectorAll(".square")
+
+    for (let i = 0; i < squares.length; i += 1) {
+        squares[i].style.backgroundColor = colors[i];     
+    }
 
 })(document);
