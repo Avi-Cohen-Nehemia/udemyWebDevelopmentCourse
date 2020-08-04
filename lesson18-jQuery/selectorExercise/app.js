addd26f92994288161b94e5e13f2d4b67284fbda
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-04 15:39:16 +0100

    finished selectorExercise

--- lesson18-jQuery/selectorExercise/app.js
+++ lesson18-jQuery/selectorExercise/app.js
@@ -1,8 +1,15 @@
 ((d) => {
 
+    // change all divs background to purple
     $("div").css("background-color", "purple");
+
+    // give all divs with class of highlight 200px width
     $(".highlight").css("width", "200px");
+
+    // give the div with id of "third" an orange color
     $("#third").css("border", "2px solid orange");
+
+    // change the text color of the first div to pink
     $("div:nth-child(1)").css("color", "pink");
 
 })(document);
