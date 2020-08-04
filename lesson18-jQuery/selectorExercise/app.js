44048dcd49af109860719b4b63a54322e52b83a0
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-04 15:35:54 +0100

    finished selectorExercise

--- lesson18-jQuery/selectorExercise/app.js
+++ lesson18-jQuery/selectorExercise/app.js
@@ -1,5 +1,8 @@
 ((d) => {
 
-
+    $("div").css("background-color", "purple");
+    $(".highlight").css("width", "200px");
+    $("#third").css("border", "2px solid orange");
+    $("div:nth-child(1)").css("color", "pink");
 
 })(document);
