a21d22f1bf606060c9a669dfca3efe7648434892
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-05 16:40:14 +0100

    items can be crossed off or uncrossed when clicked on using toggleClass

--- lesson20-todoList/assets/js/app.js
+++ lesson20-todoList/assets/js/app.js
@@ -2,17 +2,7 @@
 
     // check/uncheck specific items as completed
     $("li").on("click", function(){
-        if ($(this).css("color") !== "rgb(128, 128, 128)") {
-            $(this).css({
-                color: "gray",
-                textDecoration: "line-through",
-            });
-        } else {
-            $(this).css({
-                color: "black",
-                textDecoration: "none",
-            });
-        }
+        $(this).toggleClass("completed");
     });
 
 })(document);
