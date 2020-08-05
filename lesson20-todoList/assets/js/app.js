83cde308bbc7ae5bb1591f9ef85cfe837950ba0a
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-05 17:57:01 +0100

    added styling to the list items, h1, input and container

--- lesson20-todoList/assets/js/app.js
+++ lesson20-todoList/assets/js/app.js
@@ -1,22 +1,36 @@
 ((d) => {
 
     // check/uncheck specific items as completed
-    $("li").on("click", function() {
+    // we are using the "on" method and specifying "li" after "click" to take into account any future list items that we will add
+    $(".list").on("click", "li", function() {
         $(this).toggleClass("completed");
     });
 
     // delete a specific item from the list
-    $(".delete").on("click", function(e){
-
+    $(".list").on("click", ".removeItem", function(e){
         // fadeOut the item
         $(this).parent().fadeOut(400, function() {
-            
             // remove the item when it finished fading
             $(this).remove();
         });
-
         // use stopPropagtion to prevent bubbling
         e.stopPropagation();
+    });
+
+    // add a new item to the list
+    $("input[type='text']").on("keypress", function(e) {
+
+        // if 'enter' was pressed
+        if (e.which === 13) {
+            // save the text value
+            let itemText = $(this).val();
+            // append a new li with the value above
+            $(".list").append("<li>"+itemText+" <span>X</span></li>");
+            // add the class removeItem to the span
+            $(".list li span").last().addClass("removeItem");
+            // clear the input
+            $(this).val("");
+        }
     });
 
 })(document);
