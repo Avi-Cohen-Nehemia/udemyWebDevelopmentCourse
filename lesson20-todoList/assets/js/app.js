0c951631c2f9c90ee4edf19525176204e58aac04
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-05 16:35:17 +0100

    items can be crossed off or uncrossed when clicked on

--- lesson20-todoList/assets/js/app.js
+++ lesson20-todoList/assets/js/app.js
@@ -1,3 +1,18 @@
 ((d) => {
 
+    // check/uncheck specific items as completed
+    $("li").on("click", function(){
+        if ($(this).css("color") !== "rgb(128, 128, 128)") {
+            $(this).css({
+                color: "gray",
+                textDecoration: "line-through",
+            });
+        } else {
+            $(this).css({
+                color: "black",
+                textDecoration: "none",
+            });
+        }
+    });
+
 })(document);
