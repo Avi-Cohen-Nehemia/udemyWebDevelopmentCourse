ed677dad1f8fa2d9bcd5dc10d2429750b6d80441
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-05 16:52:26 +0100

    items fade and removed when clicking on the x

--- lesson20-todoList/assets/js/app.js
+++ lesson20-todoList/assets/js/app.js
@@ -1,8 +1,22 @@
 ((d) => {
 
     // check/uncheck specific items as completed
-    $("li").on("click", function(){
+    $("li").on("click", function() {
         $(this).toggleClass("completed");
+    });
+
+    // delete a specific item from the list
+    $(".delete").on("click", function(e){
+
+        // fadeOut the item
+        $(this).parent().fadeOut(400, function() {
+            
+            // remove the item when it finished fading
+            $(this).remove();
+        });
+
+        // use stopPropagtion to prevent bubbling
+        e.stopPropagation();
     });
 
 })(document);
