048e6c19dc354e4d62260e9655b6abb69d85072c
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-08 14:09:45 +0100

    stored methods in variables

--- lesson24-nodeJS/npmFakerExercise/listProducts.js
+++ lesson24-nodeJS/npmFakerExercise/listProducts.js
@@ -3,5 +3,8 @@ let faker = require("faker");
 
 // print 10 random products and prices
 for (let i = 1; i <= 10; i += 1) {
-    console.log(faker.commerce.productName() + ", " + faker.commerce.price());
+    let product = faker.commerce.productName();
+    let price = faker.commerce.price();
+
+    console.log(product + ", " + price);
 }
