6d1e6a3f75b5e926883811e8b069c8b82f9b05db
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-10 16:06:43 +0100

    added and made use of route parameters

--- lesson25-severSideFrameworks/express/app.js
+++ lesson25-severSideFrameworks/express/app.js
@@ -16,6 +16,17 @@ app.get("/dog", (req, res) => {
     res.send("MEWO!");
 });
 
+// using parameters in routes
+app.get("/r/:subredditName", (req, res) => {
+    let subreddit = req.params.subredditName.toUpperCase();
+    // customize using parameters
+    res.send("WELCOME TO THE " + subreddit + " SUBREDDIT");
+});
+// another example of using parameters
+app.get("/r/:subredditName/comments/:id/:title", (req, res) => {
+    res.send("Welcome to the comments section!");
+});
+
 // when user tryes to access a non existing page we can use * to show them a 404 page
 // you should define this route at the end of your routes so it won't override the others
 app.get("*", (req, res) => {
