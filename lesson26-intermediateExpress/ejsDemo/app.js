bfcbfb3519df7214bdc7a5f882a5bbc7b5ccb067
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-11 11:27:43 +0100

    started intermidateExpress lesson

--- lesson26-intermediateExpress/ejsDemo/app.js
+++ lesson26-intermediateExpress/ejsDemo/app.js
@@ -5,6 +5,11 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
+app.get("/fall-in-love-with/:something", (req, res) => {
+    let something = req.params.something;
+    res.render("love.ejs", {somethingVar: something});
+});
+
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
