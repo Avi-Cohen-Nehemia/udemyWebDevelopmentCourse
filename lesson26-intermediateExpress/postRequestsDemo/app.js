e490be5fe5bd73e9a36e7eebff674d2422d789f8
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-11 13:36:44 +0100

    added a post request that creates a new friend

--- lesson26-intermediateExpress/postRequestsDemo/app.js
+++ lesson26-intermediateExpress/postRequestsDemo/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+let friends = ["Tony", "Miranda", "Justin", "Pierre", "Lilly"];
 
 app.get("/", (req, res) => {
     res.render("home");
+});
+
+app.get("/friends", (req, res) => {
+    res.render("friends", {friends: friends});
+});
+
+app.post("/addFriend", (req, res) => {
+    let newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect("friends");
 });
 
 app.listen(3000, () => {
