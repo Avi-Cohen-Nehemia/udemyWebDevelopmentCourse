5850fbaf1dcf7e1899ac8192d3adadb38663e902
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-11 11:55:34 +0100

    added posts page views

--- lesson26-intermediateExpress/ejsDemo/app.js
+++ lesson26-intermediateExpress/ejsDemo/app.js
@@ -5,9 +5,19 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
+// pass a variable to a ejs/template file in the "views" directory
 app.get("/fall-in-love-with/:something", (req, res) => {
     let something = req.params.something;
-    res.render("love.ejs", {somethingVar: something});
+    res.render("love.ejs", {something: something});
+});
+
+app.get("/posts", (req, res) => {
+    let posts = [
+        {title: "post 1", author: "Susy"},
+        {title: "My Adorable Pet", author: "Charlie"},
+        {title: "Can you believe this pomsky?", author: "Colt"},
+    ];
+    res.render("posts.ejs", {posts: posts});
 });
 
 app.listen(3000, () => {
