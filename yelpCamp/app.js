ccc7e975833980f80db228043ba3e09ec6bd38cf
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-13 10:59:48 +0100

    form is now submitting a new campground to the campgrounds array

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -8,6 +8,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 // make express process views as ejs files by default
 app.set("view engine", "ejs");
 
+let campgrounds = [
+    {
+        name: "Salmon Creek",
+        image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        name: "Granite Hill",
+        image: "https://images.unsplash.com/photo-1473713984581-b8918cc3652e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        name: "Mountain Goat's Rest",
+        image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    },
+];
+
 // route for creating a new campground
 app.get("/campgrounds/new", (req, res) => {
     res.render("newCampground");
@@ -15,20 +30,6 @@ app.get("/campgrounds/new", (req, res) => {
 
 // route for showing all campground
 app.get("/campgrounds", (req, res) => {
-    let campgrounds = [
-        {
-            name: "Salmon Creek",
-            image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        },
-        {
-            name: "Granite Hill",
-            image: "https://images.unsplash.com/photo-1473713984581-b8918cc3652e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        },
-        {
-            name: "Mountain Goat's Rest",
-            image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-        },
-    ];
     res.render("campgrounds", {campgrounds: campgrounds});
 });
 
@@ -39,9 +40,17 @@ app.get("/", (req, res) => {
 
 // route for creating a new campground
 app.post("/campgrounds", (req, res) => {
-    // get data from the form
+    // get the data from the form
+    let name = req.body.name;
+    let image = req.body.image;
     // add the new campground to the campgrounds array
+    let newCampground = {
+        name: name,
+        image: image,
+    }
+    campgrounds.push(newCampground);
     // redirect to the campgrounds page
+    res.redirect("/campgrounds");
 });
 
 // set a port for the server to start on
