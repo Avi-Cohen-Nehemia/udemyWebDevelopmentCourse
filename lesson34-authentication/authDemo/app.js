ba34cf8907e49651075533a8cc5bb78cda9d0b7c
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-19 14:46:15 +0100

    imported mongoose and connection to data base

--- lesson34-authentication/authDemo/app.js
+++ lesson34-authentication/authDemo/app.js
@@ -1,4 +1,12 @@
 const express = require("express");
+
+const mongoose = require("mongoose");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb://localhost:27017/auth_demo')
+	.then(() => console.log('Connected to DB!'))
+	.catch(error => console.log(error.message));
+
 const app = express();
 app.set("view engine", "ejs");
 
