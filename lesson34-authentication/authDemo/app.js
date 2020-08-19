01f1c07faf2ab530fe6adbe1c758902d0c9f97bb
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-19 14:44:05 +0100

    installed nodemon and created a 'secret' view

--- lesson34-authentication/authDemo/app.js
+++ lesson34-authentication/authDemo/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
-app.set("view engin", "ejs");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("home");
+});
+
+app.get("/secret", (req, res) => {
+    res.render("secret");
+});
+
+app.listen(3000, () => {
+    console.log("Auth Demo app started on port 3000");
 });
