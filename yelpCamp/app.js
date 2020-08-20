96120290ca4ac63d9fb99260a04297ef6b9af4dd
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-20 13:58:46 +0100

    changed comments and tidied up app.js

--- yelpCamp/app.js
+++ yelpCamp/app.js
@@ -1,7 +1,7 @@
 // ==========================
 //          SETUP
 // ==========================
-// import packages
+// require packages
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -10,17 +10,17 @@ const LocalStrategy = require("passport-local");
 const expressSession = require("express-session");
 const axios = require('axios');
 
-//import routes
+// require routes
 const campgroundRoutes = require("./routes/campgrounds");
 const commentRoutes = require("./routes/comments");
 const indexRoutes = require("./routes/index");
 
-// import models
+// require models
 const Campground = require("./models/campground");
 const Comment = require("./models/comment");
 const User = require("./models/user");
 
-// import mongoose and connect to db
+// require mongoose and connect to db
 const mongoose = require("mongoose");
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
@@ -28,7 +28,7 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp')
 	.then(() => console.log('Connected to DB!'))
 	.catch(error => console.log(error.message));
 
-// import the seeding and seed the data base
+// require the seeding and seed the data base
 // const seedDB = require("./seeds");
 // seedDB();
 
@@ -44,12 +44,6 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// middleware to make user information available to all routes
-app.use((req, res, next) => {
-	res.locals.currentUser = req.user;
-	next();
-});
-
 // tell express to use body parser
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -58,6 +52,12 @@ app.set("view engine", "ejs");
 
 // tell express to serve the public directory
 app.use(express.static("./public"));
+
+// middleware to make user information available to all routes
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
 
 // tell app.js to use these route files which need to be declared
 // at the bottom of app.js just before the listen method
