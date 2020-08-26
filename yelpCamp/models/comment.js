525affc98e0d5484fceb342258db203bc4f70f2d
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-08-26 09:58:25 +0100

    added createdAt property to the comment model

--- yelpCamp/models/comment.js
+++ yelpCamp/models/comment.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
  
 const CommentSchema = new mongoose.Schema({
     text: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
     author:{
         id: {
             type: mongoose.Schema.Types.ObjectId,
